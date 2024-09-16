cd9a3d067d472ebbff91b1d8bc0183f3788f8bbb
cabbelln4806 <cabbelln4806@student.faytechcc.edu>
2024-09-16 11:11:39 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//CSC 134
+//M1HW1- Movie Talk
+//Nia Cabbell
+//8/28/2024
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+string movie;
+int year;
+double boxoffice;
+  
+
+cout<< "The movie ";
+  cin >> movie;
+cout<< "was came out the year ";
+  cin >> year;
+cout<< "and the box office gross was ";
+  cin >> boxoffice;
+cout<< "There are two quotes from the movie I will be using are" << endl;
+cout<< " 1) You probably think this world is a dream come true... but you're wrong- cat" << endl;
+cout<< "2) How can you walk away from something and still come back to it?" << endl;
+    }
